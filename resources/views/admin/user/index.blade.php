34411492c6388eeebd41ee4531abab10c0e30f7a
Achitaphol Assawadetmatakul <man.achitaphol@gmail.com>
2023-08-09 14:29:05 +0700

    up/9/8/23_no.2

---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,3 @@
+@extends('layouts.masterbackend')
+@section('text')
+@endsection
--- resources/views/layouts/masterbackend.blade.php
+++ resources/views/layouts/masterbackend.blade.php
@@ -31,11 +31,11 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('dashboard')}}">
                 <div class="sidebar-brand-icon rotate-n-15">
 
                 </div>
-                <div class="sidebar-brand-text mx-3">Pakkred Hut Admin</div>
+                <div   div class="sidebar-brand-text mx-3">Pakkred Hut Admin</div>          
             </a>
 
             <!-- Divider -->
@@ -43,7 +43,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="{{route('dashboard')}}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -51,7 +51,7 @@
                 <hr class="sidebar-divider my-0">
             <!-- Nav Item - User -->
             <li class="nav-item active">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="{{route('us_index')}}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>User</span></a>
             </li>
